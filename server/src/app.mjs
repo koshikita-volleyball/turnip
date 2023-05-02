8ea3af7ebcd0bc4586bf3ed73bc80c4229b71da0
osawa-koki <github@osawa.dev>
2023-05-02 22:00:23 +0900

    CORSヘッダを返した。

--- server/src/app.mjs
+++ server/src/app.mjs
@@ -15,6 +15,11 @@ export const lambdaHandler = async (event, context) => {
   try {
     return {
       'statusCode': 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
       'body': JSON.stringify({
         message: 'hello world',
       })
