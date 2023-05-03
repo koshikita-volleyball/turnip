ef1260c3d8e5e14087b7feec7bf435024a1e9446
osawa-koki <github@osawa.dev>
2023-05-03 10:38:32 +0900

    JQuantsクライアントにパラメタを指定できるようにした。

--- server/src/common/jquants_client.ts
+++ server/src/common/jquants_client.ts
@@ -1,9 +1,12 @@
 import GetIdToken from "./get_id_token"
 import { base_uri } from "./const"
 
-async function JQuantsClient<T>(path: string): Promise<T> {
+async function JQuantsClient<T>(path: string, params: {[key: string]: string} | null = null): Promise<T> {
   const id_token = await GetIdToken();
-  const response = await fetch(`${base_uri}${path}`, {
+  const query_string = params
+    ? '?' + Object.keys(params).map(key => `${key}=${params[key]}`).join("&")
+    : ''
+  const response = await fetch(`${base_uri}${path}${query_string}`, {
     method: 'GET',
     headers: {
       'Authorization': `Bearer ${id_token}`,
