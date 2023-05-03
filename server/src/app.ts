0794c19eec382d5c31e5fb8e3ece4480be4f08a3
s-usukura <81461525+s-usukura@users.noreply.github.com>
2023-05-03 18:32:20 +0900

    朝７時にslack送信するように変更

--- server/src/app.ts
+++ server/src/app.ts
@@ -151,7 +151,7 @@ export const slack_notify_handler = async (event: any, context: any) => {
 
   const channel = process.env.SLACK_NOTICE_CHANNEL
   const result = await slackClient.chat.postMessage({
-    text: '`/api/slack`にアクセスあった時 or 1分毎にトリ :tori:',
+    text: '朝７時だよ :tori:',
     channel,
   });
 
