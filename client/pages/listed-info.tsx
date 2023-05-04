395c2377a627e1417a502eb4dffc9dc31dea113b
osawa-koki <github@osawa.dev>
2023-05-05 04:29:48 +0900

    名前による絞り込みまで。

--- client/pages/listed-info.tsx
+++ client/pages/listed-info.tsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 import useSWR from 'swr'
 import setting from '../setting'
-import { Alert, Button, Spinner, Table } from 'react-bootstrap'
+import { Alert, Button, Form, Spinner, Table } from 'react-bootstrap'
 import ListedInfoStruct from '../interface/listed_info'
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 export default function AboutPage() {
   const [page, setPage] = useState(1)
+  const [useCondition, setUseCondition] = useState(false)
+  const [company_name, setCompanyName] = useState('')
 
   const {
     data,
@@ -16,12 +18,16 @@ export default function AboutPage() {
   }: {
     data: ListedInfoStruct[]
     error: any
-  } = useSWR(`${setting.apiPath}/api/listed_info?page=${page}`, fetcher, {
+  } = useSWR(
+    `${setting.apiPath}/api/listed_info?page=${page + 1}${useCondition === false ? '' : company_name !== '' ? `&company_name=${company_name}` : ''}`
+  , fetcher, {
     revalidateOnFocus: false,
     dedupingInterval: 10000,
   })
 
-  useSWR(`${setting.apiPath}/api/listed_info?page=${page + 1}`, fetcher, {
+  useSWR(
+    `${setting.apiPath}/api/listed_info?page=${page + 1}${useCondition === false ? '' : company_name !== '' ? `&company_name=${company_name}` : ''}`
+  , fetcher, {
     revalidateOnFocus: false,
     dedupingInterval: 10000,
   })
@@ -98,6 +104,26 @@ export default function AboutPage() {
             </Table>
           </>
         )}
+        <div className='mt-3'>
+        {
+          useCondition
+          ? <Button variant="secondary" size='sm' onClick={() => setUseCondition(false)}>条件を指定しない</Button>
+          : <Button variant="secondary" size='sm' onClick={() => setUseCondition(true)}>条件を指定して検索</Button>
+        }
+        </div>
+        {
+          useCondition && <div className='mt-3 p-3 bg-light border'>
+            <Form>
+              <Form.Group className='mt-3'>
+                <Form.Label>銘柄名</Form.Label>
+                <Form.Control type="text" placeholder="銘柄名" value={company_name} onChange={(e) => {
+                  setCompanyName(e.target.value)
+                  setPage(1)
+                }} />
+              </Form.Group>
+            </Form>
+          </div>
+        }
       </div>
     </Layout>
   )
