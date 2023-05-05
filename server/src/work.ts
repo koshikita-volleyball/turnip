697528f7a9693ce452e3b66afe4a7616691cb494
osawa-koki <github@osawa.dev>
2023-05-05 17:28:43 +0900

    Revert "小さな修正。"
    This reverts commit 71afeeb6cc55e72ecb295cbfdb1367541eb0c2f6.

--- server/src/work.ts
+++ server/src/work.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 
 export const work_stock_prices_importer_to_dynamodb_handler = async (): Promise<void> => {
-  console.log('work_stock_prices_importer_to_dynamodb_handler');
 }
