71afeeb6cc55e72ecb295cbfdb1367541eb0c2f6
osawa-koki <github@osawa.dev>
2023-05-05 16:40:46 +0900

    小さな修正。

--- server/src/work.ts
+++ server/src/work.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 
 export const work_stock_prices_importer_to_dynamodb_handler = async (): Promise<void> => {
+  console.log('work_stock_prices_importer_to_dynamodb_handler');
 }
