9dedad294dd1808bddbc0ee850ce41ab619aebac
osawa-koki <github@osawa.dev>
2023-05-05 23:31:48 +0900

    チャートの表示スタイル調整した。

--- client/components/CompanyPriceChart.tsx
+++ client/components/CompanyPriceChart.tsx
@@ -4,6 +4,11 @@ import { Line } from 'react-chartjs-2'
 ChartJS.register(...registerables);
 import PricesDailyQuotesStruct from "../interface/prices_daily_quotes"
 
+const dataset_setting = {
+  borderWidth: 1,
+  radius: 1,
+}
+
 export default function CompanyPriceChart(props: {prices: PricesDailyQuotesStruct[]}) {
 
   if (!props.prices) {
@@ -18,21 +23,25 @@ export default function CompanyPriceChart(props: {prices: PricesDailyQuotesStruc
         label: "高値",
         data: props.prices.map((item) => item.High),
         borderColor: "red",
+        ...dataset_setting,
       },
       {
         label: "低値",
         data: props.prices.map((item) => item.Low),
         borderColor: "blue",
+        ...dataset_setting,
       },
       {
         label: "始値",
         data: props.prices.map((item) => item.Open),
         borderColor: "green",
+        ...dataset_setting,
       },
       {
         label: "終値",
         data: props.prices.map((item) => item.Close),
         borderColor: "orange",
+        ...dataset_setting,
       },
     ],
   };
