b602db3c0902658fd40d72ea66aaa6818d27fbe1
osawa-koki <github@osawa.dev>
2023-05-05 17:28:15 +0900

    Revert "yran check fix"
    This reverts commit 4f034dc2147fc0dc331bac3559b27c4e3b27733d.

--- server/src/work.ts
+++ server/src/work.ts
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-empty-function */
 
 export const work_stock_prices_importer_to_dynamodb_handler = async (): Promise<void> => {
-  console.log('work_stock_prices_importer_to_dynamodb_handler')
+  console.log('work_stock_prices_importer_to_dynamodb_handler');
 }
