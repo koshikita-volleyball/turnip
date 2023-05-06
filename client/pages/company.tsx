e46d79a7d88a07bb8a05e0948d6a5cf8e7ad89e0
osawa-koki <github@osawa.dev>
2023-05-06 23:53:34 +0900

    yarn prettify

--- client/pages/company.tsx
+++ client/pages/company.tsx
@@ -7,13 +7,14 @@ import { Alert, Spinner, Table } from 'react-bootstrap'
 import CompanyPriceChart from '../components/CompanyPriceChart'
 import PricesDailyQuotesStruct from '../interface/prices_daily_quotes'
 
-const fetcher = (url: string) => fetch(url).then((r) => {
-  if (r.ok) {
-    return r.json()
-  } else {
-    throw null
-  }
-})
+const fetcher = (url: string) =>
+  fetch(url).then((r) => {
+    if (r.ok) {
+      return r.json()
+    } else {
+      throw null
+    }
+  })
 
 export default function Company() {
   const [code, setCode] = useState<string>('')
@@ -104,17 +105,16 @@ export default function Company() {
                 </>
               )
             })()}
-            {
-              prices_error ? (
-                <Alert variant="danger">Failed to load...</Alert>
-              ) : (
-                prices ? <CompanyPriceChart prices={prices} />
-                  : <Alert variant="secondary" className='d-flex align-items-center'>
-                    <Spinner animation="grow" variant="primary" className='me-3' />
-                    株価データを取得中...
-                  </Alert>
-              )
-            }
+            {prices_error ? (
+              <Alert variant="danger">Failed to load...</Alert>
+            ) : prices ? (
+              <CompanyPriceChart prices={prices} />
+            ) : (
+              <Alert variant="secondary" className="d-flex align-items-center">
+                <Spinner animation="grow" variant="primary" className="me-3" />
+                株価データを取得中...
+              </Alert>
+            )}
           </div>
         )}
       </div>
