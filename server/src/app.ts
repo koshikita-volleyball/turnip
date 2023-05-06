0b3ded0c391ec4611858d9e8bf8034d5081bf73e
s-usukura <81461525+s-usukura@users.noreply.github.com>
2023-05-06 12:56:03 +0900

    dynamoから銘柄一覧を取得する関数の作成

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,9 +13,10 @@ import AWS from './common/aws'
 import GetIdToken from './common/get_id_token'
 import GetProcessEnv from './common/process_env'
 import { ExpressionAttributeValueMap } from 'aws-sdk/clients/dynamodb'
-import { per_page } from './common/const'
+// import { per_page } from './common/const'
 import { notify } from './common/slack'
 import dayjs from './common/dayjs'
+import { getStocks } from './model/stock'
 
 dotenv.config()
 
@@ -81,95 +82,14 @@ export const business_day_update_handler = async (): Promise<void> => {
   }
 }
 
-export const listed_info_handler = async (
-  event: APIGatewayEvent,
-): Promise<APIGatewayProxyResult> => {
+export const listed_info_handler = async (): // event: APIGatewayEvent,
+Promise<APIGatewayProxyResult> => {
   try {
-    // パラメタを取得
-    const page = parseInt(event.queryStringParameters?.page ?? '1')
-    const code = event.queryStringParameters?.code
-    const company_name = event.queryStringParameters?.company_name
-    const sector_17_code = event.queryStringParameters?.sector_17_code
-    const sector_33_code = event.queryStringParameters?.sector_33_code
-    const market_code = event.queryStringParameters?.market_code
-
-    // DynamoDBから銘柄情報を取得
-    const dynamodb = new AWS.DynamoDB()
-
-    if (code) {
-      const params = {
-        TableName: GetProcessEnv('LISTED_INFO_DYNAMODB_TABLE_NAME'),
-        KeyConditionExpression: 'Code = :Code',
-        ExpressionAttributeValues: {
-          ':Code': {
-            S: code,
-          },
-        },
-      }
-      const stocks = ((await dynamodb.query(params).promise()).Items || []).map(
-        item => unmarshall(item) as ListedInfoStruct,
-      )
-      return {
-        statusCode: 200,
-        headers: CORS_HEADERS,
-        body: JSON.stringify(stocks),
-      }
-    } else {
-      // 全件スキャン
-      // ひとつ以上のフィルターを指定する必要があるため、ダミーのフィルター(銘柄コードが0000以外 | 全て)を指定しておく。
-      const filter_expressions = ['Code <> :Code']
-      const expression_attribute_values: ExpressionAttributeValueMap = {
-        ':Code': {
-          S: '0000',
-        },
-      }
-      if (sector_17_code) {
-        filter_expressions.push('Sector17Code = :Sector17Code')
-        expression_attribute_values[':Sector17Code'] = {
-          S: sector_17_code,
-        }
-      }
-      if (sector_33_code) {
-        filter_expressions.push('Sector33Code = :Sector33Code')
-        expression_attribute_values[':Sector33Code'] = {
-          S: sector_33_code,
-        }
-      }
-      if (market_code) {
-        filter_expressions.push('MarketCode = :MarketCode')
-        expression_attribute_values[':MarketCode'] = {
-          S: market_code,
-        }
-      }
-      const params = {
-        TableName: GetProcessEnv('LISTED_INFO_DYNAMODB_TABLE_NAME'),
-        FilterExpression: filter_expressions.join(' AND '),
-        ExpressionAttributeValues: expression_attribute_values,
-      }
-      const stocks = ((await dynamodb.scan(params).promise()).Items || []).map(
-        item => unmarshall(item) as ListedInfoStruct,
-      )
-
-      // 銘柄名でフィルタリング
-      const filtered_stocks = stocks.filter(stock => {
-        return company_name ? stock.CompanyName.includes(company_name) : true
-      })
-
-      // 銘柄コードでソート
-      filtered_stocks.sort((a, b) => {
-        return a.Code.localeCompare(b.Code)
-      })
-
-      // ページング
-      const start = (page - 1) * per_page
-      const end = start + per_page
-      const paged_stocks = filtered_stocks.slice(start, end)
-
-      return {
-        statusCode: 200,
-        headers: CORS_HEADERS,
-        body: JSON.stringify(paged_stocks),
-      }
+    const stocks = await getStocks()
+    return {
+      statusCode: 200,
+      headers: CORS_HEADERS,
+      body: JSON.stringify(stocks),
     }
   } catch (err: unknown) {
     if (err instanceof Error) {
@@ -183,6 +103,106 @@ export const listed_info_handler = async (
       }),
     }
   }
+
+  // try {
+  //   // パラメタを取得
+  //   const page = parseInt(event.queryStringParameters?.page ?? '1')
+  //   const code = event.queryStringParameters?.code
+  //   const company_name = event.queryStringParameters?.company_name
+  //   const sector_17_code = event.queryStringParameters?.sector_17_code
+  //   const sector_33_code = event.queryStringParameters?.sector_33_code
+  //   const market_code = event.queryStringParameters?.market_code
+
+  //   // DynamoDBから銘柄情報を取得
+  //   const dynamodb = new AWS.DynamoDB()
+
+  //   if (code) {
+  //     const params = {
+  //       TableName: GetProcessEnv('LISTED_INFO_DYNAMODB_TABLE_NAME'),
+  //       KeyConditionExpression: 'Code = :Code',
+  //       ExpressionAttributeValues: {
+  //         ':Code': {
+  //           S: code,
+  //         },
+  //       },
+  //     }
+  //     const stocks = ((await dynamodb.query(params).promise()).Items || []).map(
+  //       item => unmarshall(item) as ListedInfoStruct,
+  //     )
+  //     return {
+  //       statusCode: 200,
+  //       headers: CORS_HEADERS,
+  //       body: JSON.stringify(stocks),
+  //     }
+  //   } else {
+  //     // 全件スキャン
+  //     // ひとつ以上のフィルターを指定する必要があるため、ダミーのフィルター(銘柄コードが0000以外 | 全て)を指定しておく。
+  //     const filter_expressions = ['Code <> :Code']
+  //     const expression_attribute_values: ExpressionAttributeValueMap = {
+  //       ':Code': {
+  //         S: '0000',
+  //       },
+  //     }
+  //     if (sector_17_code) {
+  //       filter_expressions.push('Sector17Code = :Sector17Code')
+  //       expression_attribute_values[':Sector17Code'] = {
+  //         S: sector_17_code,
+  //       }
+  //     }
+  //     if (sector_33_code) {
+  //       filter_expressions.push('Sector33Code = :Sector33Code')
+  //       expression_attribute_values[':Sector33Code'] = {
+  //         S: sector_33_code,
+  //       }
+  //     }
+  //     if (market_code) {
+  //       filter_expressions.push('MarketCode = :MarketCode')
+  //       expression_attribute_values[':MarketCode'] = {
+  //         S: market_code,
+  //       }
+  //     }
+  //     const params = {
+  //       TableName: GetProcessEnv('LISTED_INFO_DYNAMODB_TABLE_NAME'),
+  //       FilterExpression: filter_expressions.join(' AND '),
+  //       ExpressionAttributeValues: expression_attribute_values,
+  //     }
+  //     const stocks = ((await dynamodb.scan(params).promise()).Items || []).map(
+  //       item => unmarshall(item) as ListedInfoStruct,
+  //     )
+
+  //     // 銘柄名でフィルタリング
+  //     const filtered_stocks = stocks.filter(stock => {
+  //       return company_name ? stock.CompanyName.includes(company_name) : true
+  //     })
+
+  //     // 銘柄コードでソート
+  //     filtered_stocks.sort((a, b) => {
+  //       return a.Code.localeCompare(b.Code)
+  //     })
+
+  //     // ページング
+  //     const start = (page - 1) * per_page
+  //     const end = start + per_page
+  //     const paged_stocks = filtered_stocks.slice(start, end)
+
+  //     return {
+  //       statusCode: 200,
+  //       headers: CORS_HEADERS,
+  //       body: JSON.stringify(paged_stocks),
+  //     }
+  //   }
+  // } catch (err: unknown) {
+  //   if (err instanceof Error) {
+  //     console.error(`[ERROR] ${err.message}`)
+  //   }
+  //   return {
+  //     statusCode: 500,
+  //     headers: CORS_HEADERS,
+  //     body: JSON.stringify({
+  //       message: err,
+  //     }),
+  //   }
+  // }
 }
 
 export const prices_daily_quotes_handler = async (
