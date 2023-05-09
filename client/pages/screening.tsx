60e096c35de6f32595f1115b4a107f8e971f4b89
osawa-koki <github@osawa.dev>
2023-05-10 00:23:12 +0900

    fetch先のuriの作成まで。

--- client/pages/screening.tsx
+++ client/pages/screening.tsx
@@ -7,8 +7,40 @@ import ScreeningRuleModal from '../components/ScreeningRuleModal'
 import { Button } from 'react-bootstrap'
 import ScreeningRuleList from '../components/ScreeningRuleList'
 import FilteringBlock from '../components/FilteringBlock'
+import setting from '../setting'
 
 Modal.setAppElement('#Modal')
+
+function make_uri(
+  filtering: {
+    company_name: string
+    market_code: string
+    sector_17_code: string
+    sector_33_code: string
+  },
+  rules: ScreeningRuleStructs[]
+  ) {
+  const {
+    company_name,
+    market_code,
+    sector_17_code,
+    sector_33_code,
+  } = filtering
+
+  rules.forEach((rule) => {
+    delete rule.collapsed
+  })
+
+  return (
+    `${setting.apiPath}/api/screener` +
+    `?` +
+    `${company_name !== '' ? `&company_name=${company_name}` : ''}` +
+    `${market_code !== '' ? `&market_codes=${market_code}` : ''}` +
+    `${sector_17_code !== '' ? `&sector_17_codes=${sector_17_code}` : ''}` +
+    `${sector_33_code !== '' ? `&sector_33_codes=${sector_33_code}` : ''}` +
+    `${rules.length !== 0 ? `&conditions=${encodeURI(JSON.stringify(rules, null, 0))}` : ''}`
+  )
+}
 
 export default function ContactPage() {
   const [useFiltering, setUseFiltering] = useState(true)
@@ -23,6 +55,19 @@ export default function ContactPage() {
 
   const openModal = () => setIsOpen(true)
   const closeModal = () => setIsOpen(false)
+
+  const StartScreening = () => {
+    const uri = make_uri(
+      {
+        company_name,
+        market_code,
+        sector_17_code,
+        sector_33_code,
+      },
+      rules
+    )
+    console.log(uri)
+  }
 
   return (
     <Layout>
@@ -58,6 +103,7 @@ export default function ContactPage() {
         <Button
           variant="primary"
           className="d-block mx-auto w-100 mt-3"
+          onClick={StartScreening}
         >
           スクリーニングを実行
         </Button>
