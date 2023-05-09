30109fc5b68c37f77b83d364d98c92e79761c508
osawa-koki <github@osawa.dev>
2023-05-09 22:05:56 +0900

    不要なインポートを消した。

--- client/components/FilteringBlock.tsx
+++ client/components/FilteringBlock.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, SetStateAction, useState } from 'react'
-import { Button, Form } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
 import { BsArrowsCollapse, BsArrowsExpand } from 'react-icons/bs'
 
 import { MarketInfo, Sector17Info, Sector33Info } from '../data/export'
