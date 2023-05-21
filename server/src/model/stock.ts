971130f528cb681123e5774160727517fadda20a
osawa-koki <github@osawa.dev>
2023-05-22 00:52:18 +0900

    server/src/model/stock.tsのESlint対応。

--- server/src/model/stock.ts
+++ server/src/model/stock.ts
@@ -18,21 +18,21 @@ export const getStocks = async (props?: GetStocksProps): Promise<Stock[]> => {
   const params = {
     TableName: GetProcessEnv('LISTED_INFO_DYNAMODB_TABLE_NAME')
   }
-  const stocks = (((await ddb.scan(params).promise()).Items != null) || [])
+  const stocks = (((await ddb.scan(params).promise()).Items) ?? [])
     .map(item => unmarshall(item) as Stock)
     .sort((a, b) => a.Code.localeCompare(b.Code))
 
   if (props == null) {
     return stocks
   }
-  const { codes, sector_17_codes, sector_33_codes, market_codes, company_name } = props
+  const { codes, sector_17_codes: sector17Codes, sector_33_codes: sector33Codes, market_codes: marketCodes, company_name: companyName } = props
   return stocks.filter(
     stock =>
       ((codes == null) || codes.includes(stock.Code)) &&
-      ((sector_17_codes == null) || sector_17_codes.includes(stock.Sector17Code)) &&
-      ((sector_33_codes == null) || sector_33_codes.includes(stock.Sector33Code)) &&
-      ((market_codes == null) || market_codes.includes(stock.MarketCode)) &&
-      (!company_name || stock.CompanyName.includes(company_name))
+      ((sector17Codes == null) || sector17Codes.includes(stock.Sector17Code)) &&
+      ((sector33Codes == null) || sector33Codes.includes(stock.Sector33Code)) &&
+      ((marketCodes == null) || marketCodes.includes(stock.MarketCode)) &&
+      ((companyName == null) || stock.CompanyName.includes(companyName))
   )
 }
 
