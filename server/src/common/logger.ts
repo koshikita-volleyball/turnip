e6fb1a6e416e1adba9bdf2830e8987899c745043
osawa-koki <github@osawa.dev>
2023-05-22 01:06:49 +0900

    server/src/common/logger.tsのESLint対応。

--- server/src/common/logger.ts
+++ server/src/common/logger.ts
@@ -3,33 +3,30 @@ import { notify } from './slack'
 
 type Level = 'info' | 'error'
 
-class Logger {
-  private static _format_log (level: Level, funcName: string, text: string) {
-    const datetime = dayjs().format('YYYY-MM-DD HH:mm:ss')
-    return `【${level}】\t${datetime}\t${funcName}\t${text}`
-  }
+function _formatLog (level: Level, funcName: string, text: string): string {
+  const datetime = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  return `【${level}】\t${datetime}\t${funcName}\t${text}`
+}
 
-  private static _format_slack (level: Level, funcName: string, text: string) {
-    const dateTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
-    const level_str = level === 'info' ? ':large_blue_circle: Info' : ':red_circle: Error'
-    return `【Level】${level_str}\n【時刻】${dateTime}\n【関数名】${funcName}\n【メッセージ】${text}`
-  }
+function _formatSlack (level: Level, funcName: string, text: string): string {
+  const dateTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  const levelStr = level === 'info' ? ':large_blue_circle: Info' : ':red_circle: Error'
+  return `【Level】${levelStr}\n【時刻】${dateTime}\n【関数名】${funcName}\n【メッセージ】${text}`
+}
 
-  static log (funcName: string, text: string) {
-    console.log(this._format_log('info', funcName, text))
-    notify(this._format_slack('info', funcName, text), 'info').then().catch(console.error)
-  }
+function loggerInfo (funcName: string, text: string): void {
+  console.log(_formatLog('info', funcName, text))
+  notify(_formatSlack('info', funcName, text), 'info').then().catch(console.error)
+}
 
-  static error (funcName: string, text: string) {
-    console.error(this._format_log('error', funcName, text))
-    notify(this._format_slack('error', funcName, text), 'error').then().catch(console.error)
-  }
+function loggerError (funcName: string, text: string): void {
+  console.error(_formatLog('error', funcName, text))
+  notify(_formatSlack('error', funcName, text), 'error').then().catch(console.error)
 }
 
 const THREE_BACK_QUOTES = '```'
-function makeCodeBlock (text: string) {
+function makeCodeBlock (text: string): string {
   return `${THREE_BACK_QUOTES}${text}${THREE_BACK_QUOTES}`
 }
 
-export default Logger
-export { makeCodeBlock }
+export { loggerInfo, loggerError, makeCodeBlock }
