db6f374371bea85595004151a87807bb9237cb9f
osawa-koki <koki@osawa.dev>
2023-08-16 23:26:37 +0900

    remove unused module

--- client/components/Menu.tsx
+++ client/components/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import pages from '../pages'
 import { Button } from 'react-bootstrap'
